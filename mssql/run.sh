f7adc4a1975b87513fd591c38272b5adf6f62753
Simon Wunderlin <simon.wunderlin@usb.ch>
2023-03-02 16:16:28 +0100

    fixed mssql

--- mssql/run.sh
+++ mssql/run.sh
@@ -3,5 +3,5 @@
 docker compose build
 docker compose up -d
 
-SA_PASSWORD=$(grep SA_PASSWORD docker-compose.yaml | sed -e 's/[^"]\+//; s/"//g')
-echo "sa: $SA_PASSWORD"
+#SA_PASSWORD=$(grep SA_PASSWORD docker-compose.yaml | sed -e 's/[^"]\+//; s/"//g')
+#echo "sa: $SA_PASSWORD"
--- mssql/scripts/entrypoint.sh
+++ mssql/scripts/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -m
+export SA_PASSWORD="$1"
+echo "pw: $SA_PASSWORD"
 /opt/mssql/bin/sqlservr --accept-eula & /bin/bash /src/scripts/setup_database.sh 
 fg
